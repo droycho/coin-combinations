a95363fae6a34543ea6a733c5179968d753542f9
Ashley Maceli and DJ Roychoudhury <dj@>
2016-04-21 10:34:28 -0700

    Update business logic and pass new tests for each change group

--- src/main/java/CoinCombos.java
+++ src/main/java/CoinCombos.java
@@ -1,33 +1,30 @@
 public class CoinCombos {
   public static void main(String[] arg) {}
 
-    public static Integer changeBack(Integer input) {
+    public static String changeBack(Integer input) {
       Integer quarters = 0;
       Integer dimes = 0;
       Integer nickels = 0;
       Integer pennies = 0;
       Integer remainder = 0;
 
-      if (input >= 25){
-        quarters = input / 25;
-        remainder = input % 25;
-        input = input - (quarters * 25);
-        return quarters;
-      } if (input >= 10 || remainder > 0){
-        dimes = input / 10;
-        remainder = remainder % 10;
-        input = input - (dimes * 10);
-        return dimes;
-      } if (input >= 5 || remainder > 0) {
-        nickels = input / 5;
-        remainder = remainder % 5;
-        input = input - (nickels * 5);
-        return nickels;
-      } if (input > 0) {
-        pennies = input;
-        return pennies;
-      } else {
-        return input;
+      while (input > 0) {
+        if (input >= 25) {
+          input -= 25;
+          quarters++;
+        } else if (input >= 10) {
+          input -= 10;
+          dimes++;
+        } else if (input >= 5) {
+          input -= 5;
+          nickels++;
+        } else if (input >= 1) {
+          input -= 1;
+          pennies++;
+        } else {
+          return "";
+        }
       }
+      return String.format("%d quarters, %d dimes, %d nickels, %d pennies", quarters, dimes, nickels, pennies);
     }
 }
--- src/test/java/CoinCombosTest.java
+++ src/test/java/CoinCombosTest.java
@@ -4,44 +4,30 @@ import static org.junit.Assert.*;
 public class CoinCombosTest {
 
   @Test
-  public void isChangeBack_forInputValue_inputInteger() {
-    CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 1;
-    assertEquals(expected, coinCombos.changeBack(1));
-  }
-
-  @Test
-  public void isChangeBack_forChangeGroups_inputInteger() {
-    CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 2;
-    assertEquals(expected, coinCombos.changeBack(20));
-  }
-
-  @Test
   public void isChangeBack_forQuarterGroup_inputInteger() {
     CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 3;
-    assertEquals(expected, coinCombos.changeBack(86));
+    String expected = "3 quarters, 0 dimes, 0 nickels, 0 pennies";
+    assertEquals(expected, coinCombos.changeBack(75));
   }
 
   @Test
   public void isChangeBack_forDimesGroup_inputInteger() {
     CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 1;
-    assertEquals(expected, coinCombos.changeBack(86));
+    String expected = "0 quarters, 2 dimes, 0 nickels, 0 pennies";
+    assertEquals(expected, coinCombos.changeBack(20));
   }
 
   @Test
   public void isChangeBack_forNickelsGroup_inputInteger() {
     CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 0;
-    assertEquals(expected, coinCombos.changeBack(86));
+    String expected = "0 quarters, 0 dimes, 1 nickels, 0 pennies";
+    assertEquals(expected, coinCombos.changeBack(5));
   }
 
   @Test
   public void isChangeBack_forPenniesGroup_inputInteger() {
     CoinCombos coinCombos = new CoinCombos();
-    Integer expected = 1;
-    assertEquals(expected, coinCombos.changeBack(86));
+    String expected = "0 quarters, 0 dimes, 0 nickels, 3 pennies";
+    assertEquals(expected, coinCombos.changeBack(3));
   }
 }
